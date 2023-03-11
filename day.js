3014da77d60c8b39854b61be3d676fef58db625c
Rebekah Ross <rebekahross123@gmail.com>
2023-03-11 14:56:36 -0700

    Really am trying to simplify things. I think I may have gone overboard with my inital idea, but I couldn't have known that without actually trying to make the website

--- day.js
+++ day.js
@@ -1,10 +1,50 @@
+// Get the modal
+
+let modal = document.getElementById("modalPopUp");
+
+// Get the button that opens the modal
+let btn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// function save() {
+//   const monthEl = document.querySelector("#month");
+//   localStorage.setItem("month", monthEl.value);
+//   window.location.href = "day.html";
+// }
+
+// function getPlayerName() {
+//   return localStorage.getItem("month") ?? "Mystery player";
+// }
+
+// const playerNameEl = document.querySelector(".player-name");
+// playerNameEl.textContent = this.getPlayerName();
+
 const hoursTag = document.querySelector(".hours"),
   currentDate = document.querySelector(".current-date"),
   prevNextArrow = document.querySelectorAll(".arrows span");
 // getting new date, current year and month
 let date = new Date(),
   currYear = date.getFullYear(),
-  currDay = date.getDate();
+  currDate = date.getDate();
 currMonth = date.getMonth();
 // storing full name of all months in array
 const months = [
@@ -46,19 +86,19 @@ const renderCalendar = () => {
     // creating li of next month first days
     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
   }
-  currentDate.innerText = `${months[currMonth]} ${currDay}, ${currYear}`; // passing current mon and yr as currentDate text
+  currentDate.innerText = `${months[currMonth]} ${currDate}, ${currYear}`; // passing current mon and yr as currentDate text
   daysTag.innerHTML = liTag;
 };
 
 renderCalendar();
 
-prevNextArrow.forEach((arrows) => {
+prevNextArrow.forEach((arrow) => {
   // getting prev and next icons
-  arrows.addEventListener("click", () => {
+  arrow.addEventListener("click", () => {
     // adding click event on both icons
     // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-    currMonth = arrows.id === "prev" ? currMonth - 1 : currMonth + 1;
-    if (currMonth < 0 || currMonth > 11) {
+    currDate = arrow.id === "prev" ? currDate - 1 : currDate + 1;
+    if (currDate < 1 || currDate > 11) {
       // if current month is less than 0 or greater than 11
       // creating a new date of current year & month and pass it as date value
       date = new Date(currYear, currMonth, new Date().getDate());
