d8f52b247c207f4515b2f0aa1568c6a6601294a0
Rebekah Ross <rebekahross123@gmail.com>
2023-03-11 19:46:59 -0700

    the date updates correctly so that's a big win! Next step - getting the javascript working to accept activities and load them to the right day.

--- day.js
+++ day.js
@@ -25,12 +25,6 @@ window.onclick = function (event) {
   }
 };
 
-// function save() {
-//   const monthEl = document.querySelector("#month");
-//   localStorage.setItem("month", monthEl.value);
-//   window.location.href = "day.html";
-// }
-
 // function getPlayerName() {
 //   return localStorage.getItem("month") ?? "Mystery player";
 // }
@@ -44,8 +38,9 @@ const hoursTag = document.querySelector(".hours"),
 // getting new date, current year and month
 let date = new Date(),
   currYear = date.getFullYear(),
-  currDate = date.getDate();
-currMonth = date.getMonth();
+  currDate = date.getDate(),
+  currMonth = date.getMonth(),
+  currDay = date.getDay();
 // storing full name of all months in array
 const months = [
   "January",
@@ -61,52 +56,129 @@ const months = [
   "November",
   "December",
 ];
-const renderCalendar = () => {
-  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-  let liTag = "";
-  for (let i = firstDayofMonth; i > 0; i--) {
-    // creating li of previous month last days
-    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const setDate = () => {
+  if (
+    new Date().getDate() === currDate &&
+    new Date().getFullYear() === currYear &&
+    new Date().getMonth() === currMonth
+  ) {
+    currentDate.innerText = `${days[currDay]},\n${months[currMonth]} ${currDate}, ${currYear} \n(Today)`;
+  } else {
+    currentDate.innerText = `${days[currDay]},\n${months[currMonth]} ${currDate}, ${currYear}`;
   }
-  for (let i = 1; i <= lastDateofMonth; i++) {
-    // creating li of all days of current month
-    // adding active class to li if the current day, month, and year matched
-    let isToday =
-      i === date.getDate() &&
-      currMonth === new Date().getMonth() &&
-      currYear === new Date().getFullYear()
-        ? "active"
-        : "";
-    liTag += `<li class="${isToday}">${i}</li>`;
-  }
-  for (let i = lastDayofMonth; i < 6; i++) {
-    // creating li of next month first days
-    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
-  }
-  currentDate.innerText = `${months[currMonth]} ${currDate}, ${currYear}`; // passing current mon and yr as currentDate text
-  daysTag.innerHTML = liTag;
+
+  // passing current mon and yr as currentDate text
 };
 
-renderCalendar();
+setDate();
 
 prevNextArrow.forEach((arrow) => {
   // getting prev and next icons
   arrow.addEventListener("click", () => {
     // adding click event on both icons
     // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-    currDate = arrow.id === "prev" ? currDate - 1 : currDate + 1;
-    if (currDate < 1 || currDate > 11) {
-      // if current month is less than 0 or greater than 11
-      // creating a new date of current year & month and pass it as date value
-      date = new Date(currYear, currMonth, new Date().getDate());
-      currYear = date.getFullYear(); // updating current year with new date year
-      currMonth = date.getMonth(); // updating current month with new date month
+
+    if (arrow.id === "prev") {
+      currDate = currDate - 1;
+      currDay = currDay - 1;
+      if (currDay < 0) {
+        currDay = currDay + 7;
+      }
     } else {
-      date = new Date(); // pass the current date as date value
+      currDate = currDate + 1;
+      currDay = currDay + 1;
+      if (currDay > 6) {
+        currDay = currDay - 7;
+      }
     }
-    renderCalendar(); // calling renderCalendar function
+
+    if (
+      currMonth == 0 ||
+      currMonth == 2 ||
+      currMonth == 4 ||
+      currMonth == 6 ||
+      currMonth == 7 ||
+      currMonth == 9 ||
+      currMonth == 11
+    ) {
+      if (currDate < 1 || currDate > 31) {
+        // creating a new date of current year & month and pass it as date value
+        date = new Date(currYear, currMonth, currDate, currDay);
+        currMonth = date.getMonth();
+        currYear = date.getFullYear();
+        currDate = date.getDate();
+        currDay = date.getDay();
+      } else {
+        date = new Date(); // pass the current date as date value
+      }
+      setDate();
+    } else if (currMonth === 1) {
+      if (currYear % 4 === 0) {
+        if (currDate < 1 || currDate > 29) {
+          // creating a new date of current year & month and pass it as date value
+          date = new Date(currYear, currMonth, currDate, currDay);
+          currMonth = date.getMonth();
+          currYear = date.getFullYear(); // updating current year with new date year
+          currDate = date.getDate(); // updating current month with new date month
+          currDay = date.getDay();
+        } else {
+          date = new Date(); // pass the current date as date value
+        }
+        setDate();
+      } else {
+        if (currDate < 1 || currDate > 28) {
+          // creating a new date of current year & month and pass it as date value
+          date = new Date(currYear, currMonth, currDate, currDay);
+          currMonth = date.getMonth();
+          currYear = date.getFullYear(); // updating current year with new date year
+          currDate = date.getDate(); // updating current month with new date month
+          currDay = date.getDay();
+        } else {
+          date = new Date(); // pass the current date as date value
+        }
+        setDate();
+      }
+    } else if (
+      currMonth === 3 ||
+      currMonth === 5 ||
+      currMonth === 8 ||
+      currMonth === 10
+    ) {
+      if (currDate < 1 || currDate > 30) {
+        // creating a new date of current year & month and pass it as date value
+        date = new Date(currYear, currMonth, currDate, currDay);
+        currMonth = date.getMonth();
+        currYear = date.getFullYear(); // updating current year with new date year
+        currDate = date.getDate(); // updating current month with new date month
+        currDay = date.getDay();
+      } else {
+        date = new Date(); // pass the current date as date value
+      }
+      setDate();
+    }
   });
 });
+
+function save() {
+  const activityEl = document.querySelector("#activity");
+  localStorage.setItem("activity", activityEl.value);
+  window.location.href = "day.html";
+}
+
+// function getActivity() {
+//   return localStorage.getItem("activity");
+// }
+
+// const activityEl = document.querySelector(".activity");
+// activityEl.innerHTML = this.getActivity();
