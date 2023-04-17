12e4e18cba843e74ae5b5506e05432abd0a8d515
Rebekah Ross <rebekahross123@gmail.com>
2023-04-17 13:29:50 -0600

    I added calls to third party service endpoints. At this point, I should have that, service endpoints running under node.js, authenticated login with securely stored credentials in mongoDB, and I'm not sure if that counts as storing data in MongoDB.

--- public/month.js
+++ public/month.js
@@ -69,3 +69,28 @@ prevNextArrow.forEach((arrow) => {
     renderCalendar(); // calling renderCalendar function
   });
 });
+
+function displayQuote(data) {
+  const containerEl = document.querySelector("#quote");
+
+  const quoteEl = document.createElement("p");
+  quoteEl.classList.add("quote");
+  const authorEl = document.createElement("p");
+  authorEl.classList.add("author");
+
+  quoteEl.textContent = data.content;
+  authorEl.textContent = data.author;
+
+  containerEl.appendChild(quoteEl);
+  containerEl.appendChild(authorEl);
+}
+
+function callService(url, displayCallback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      displayCallback(data);
+    });
+}
+
+callService("https://api.quotable.io/random", displayQuote);
